4b1be57dd46180b05aa05414b0d496abaa2a5bcc
Lazzzare <164561286+LazareChkhartishvili@users.noreply.github.com>
2025-09-20 15:27:13 +0400

    try to fix

--- config/cors.php
+++ config/cors.php
@@ -18,12 +18,14 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
-      'http://localhost:3000',
-      'https://greengodelivery-ge-main.vercel.app',
-      'https://greengo-api-production.up.railway.app',
-    ],
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://greengodelivery-ge-main.vercel.app',
+    'https://app-greengo-delivery-main-n7t9.vercel.app',
+    'https://greengo-api-production.up.railway.app',
+    
+],
 
     'allowed_origins_patterns' => [
       '/^https:\/\/.*\.vercel\.app$/',
